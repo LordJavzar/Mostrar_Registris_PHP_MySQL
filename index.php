74f699934400a8fd4d3922d01075049a30e06350
Francisco Carranza <javier_isic@outlook.com>
2023-06-15 19:20:09 -0600

    Primer repositorio

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registrar usuario</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="estilos.css">
+    </head>
+    <body>
+        <?php
+        include("mostrar.php");
+        ?>    
+    </body>
+    
+
+</html>
